ee1cb4bda3ed336a18894e36919ed855571126cf
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-17 22:25:17 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -23,18 +23,3 @@ class Captain < ActiveRecord::Base
   
 end
 
-
-
-#   describe "::talented_seafarers" do
-#     it "returns captains of motorboats and sailboats" do
-#       captains = ["Captain Cook", "Samuel Axe"]
-#       expect(Captain.talented_seafarers.pluck(:name)).to eq(captains)
-#     end
-#   end
-
-#   describe "::non_sailors" do
-#     it "returns people who are not captains of sailboats" do
-#       captains = ["William Kyd", "Arel English", "Henry Hudson"]
-#       expect(Captain.non_sailors.pluck(:name)).to eq(captains)
-#     end
-#   end
