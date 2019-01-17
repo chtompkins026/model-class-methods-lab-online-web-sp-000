d94253a8297a9ce10a9dc8652a9a07d1fe3c875b
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-17 22:02:03 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,11 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  #class methods 
+  
+  def self.first_five
+    all.limit(5)
+  end 
+  
 end
